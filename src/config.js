7de5e3da1837fffdf3c64dec05ce6090a0d5b161
Lynx <157897299+lynx707@users.noreply.github.com>
2024-02-12 15:50:11 +0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -18,10 +18,10 @@ module.exports = {
 
   nodes: [
     {
-      url: process.env.NODE_URL || 'lavalink.teramont.net:25565',
-      name: process.env.NODE_NAME || 'Name',
-      auth: process.env.NODE_AUTH || 'eHKuFcz67k4lBS64',
-      secure: parseBoolean(process.env.NODE_SECURE || 'false'),
+      url: process.env.NODE_URL || 'lava.alfari.id:443',
+      name: process.env.NODE_NAME || 'Plugins Lava 4.0.0 ',
+      auth: process.env.NODE_AUTH || 'youshallnotpass',
+      secure: parseBoolean(process.env.NODE_SECURE || 'true'),
     },
   ],
 };
